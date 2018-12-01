593f5653af1601e28f1cb2d69e199d7572d2a51d
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2018-12-01 01:23:01 +0100

    add pdf documentation

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
-gwtc . -n index
-gwtc locale/tr/ -o locale/tr/ -n index
-gwtc locale/es/ -o locale/es/ -n index
+#HTML
+gwtc --format html --title "LittlevGL documentation" --output . --file-name index .
+gwtc --format html --title "LittlevGL documentation" --output ./locale/tr --file-name index ./locale/tr
+gwtc --format html --title "LittlevGL documentation" --output ./locale/es --file-name index ./locale/es 
+
+#PDF
+gwtc --format pdf --pdf-page-count --title "LittlevGL documentation (English)"  --output ./pdf --file-name LittlevGL_documentation_English ./
+gwtc --format pdf --pdf-page-count --title "LittlevGL documentation (Turkish)"  --output ./pdf --file-name LittlevGL_documentation_Turkish ./locale/tr
+gwtc --format pdf --pdf-page-count --title "LittlevGL documentation (Espanol)"  --output ./pdf --file-name LittlevGL_documentation_Espanol ./locale/es
