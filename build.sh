3940e5e0b5d46349cd6ca167d6d413b1f5f46e3a
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2018-12-16 16:57:55 +0100

    regenerate documentation

--- build.sh
+++ build.sh
@@ -9,13 +9,8 @@ og='    <meta prefix="og: http://ogp.me/ns#" property="og:site_name" content="Li
 favicon='    <link rel="icon" type="image/png" sizes="32x32" href="https://littlevgl.com/favicon/favicon-32.png">\n  <link rel="icon" type="image/png" sizes="16x16" "https://littlevgl.com/favicon/favicon-16.png">\n  <link rel="apple-touch-icon" sizes="180x180" href="https://littlevgl.com/favicon/apple-touch-icon.png">'
 ga='<script async src="https://www.googletagmanager.com/gtag/js?id=UA-78811084-3"></script>\n<script>\n  window.dataLayer = window.dataLayer || [];\n  function gtag(){dataLayer.push(arguments);}\n  gtag("js", new Date());\n  gtag("config", "UA-78811084-3");\n</script>';
 
-echo "add extra <head> info to: index.html"
 sed -i  "/<head>/a  $desc\n$kw\n$og\n$favicon\n$ga" index.html
-
-echo "add extra <head> info to: tr/index.html"
 sed -i  "/<head>/a  $desc\n$kw\n$og\n$favicon\n$ga" ./locale/tr/index.html
-
-echo "add extra <head> info to: es/index.html"
 sed -i  "/<head>/a  $desc\n$kw\n$og\n$favicon\n$ga" ./locale/es/index.html
 
 #PDF
