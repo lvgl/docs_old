168c1c820dbafa6331fbf9591790bd5032fc29b8
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-07-12 13:21:42 +0200

    add zanta support

---
+++ en/trans_pull.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+langs = ['tr', 'zh-CN']
+folders = ['overview', 'get-started', 'object-types'];
+files = ['index.md'];
+
+for lang in langs:
+  for f in folders:
+    cmd = "rm -r " + "../" + lang + "/" + f
+    print(cmd)
+    os.system(cmd);
+    
+  for f in files:
+    cmd = "rm "  + "../" + lang + "/" + f
+    print(cmd)
+    os.system(cmd);
+    
+    
+cmd = "zanata-cli pull -B"
+print(cmd)
+os.system(cmd);
+
+
+for lang in langs:
+  cmd = "rename -f 's/\.txt//g' ../" + lang + "/**/*.txt"
+  print(cmd)
+  os.system(cmd);
+
+  cmd = "rename -f 's/\.txt//g' ../" + lang + "/*.txt"
+  print(cmd)
+  os.system(cmd);
+
+  
+  
---
+++ en/trans_txt/overview/index.md.txt
@@ -0,0 +1,20 @@
+# Overview
+
+```eval_rst
+
+.. toctree::
+   :maxdepth: 2
+
+   object
+   layer
+   event
+   style
+   indev
+   display
+   font
+   image
+   file-system
+   animation
+   task
+   drawing
+```
