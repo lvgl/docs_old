520454a47796efc3bcf3cd525695dd0101707ed6
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-06-16 09:23:46 +0200

    use gettext translations

--- v7/build.py
+++ v7/build.py
@@ -1,33 +1,56 @@
 #!/usr/bin/env python
-
+ 
 import sys
 import os
 
-
 langs = ['en']
 
+  
+def cmd(s):
+  print("------------------------------------------")
+  print(s)
+  print("------------------------------------------")
+  r = os.system(s)
+  if r != 0: 
+    print "Exit build due to previous error"
+    exit(-1)
+    
 clean = 0
 trans = 0
 args = sys.argv[1:]
 if len(args) == 1:
   if "clean" in args: clean = 1
-  if "trans" in args: trans = 1
-  
-if clean:
-  cmd_clean = " echo 'Clean up...\n'; rm -r html xetex; "
-else: 
-  cmd_clean = ""
+  if "trans" in args: trans = 1  
   
 if trans:
-  os.system("cd en && ./trans_push.py && ./trans_pull.py")  
-  
-cmd_pdf =   " echo '------------\nBuild PDF...\n------------'; sphinx-build -b latex . xetex && cd xetex; xelatex -interaction=batchmode *.tex; xelatex -interaction=batchmode *.tex;  cp -f LVGL.pdf ../LVGL.pdf; cd ..; "
-cmd_html =  " echo '------------\nBuild HTML...\n------------'; sphinx-build -b html . html; "
+  cmd("make -j8 gettext")
 
-cmd_build = cmd_clean + cmd_pdf + cmd_html + " "
+
+u = "sphinx-intl update -p gettext "
+for lang in langs:
+  u += " -l " + lang
+
+cmd(u)
 
 for lang in langs:
-  cmd = " echo '\n\n************\nStart " + lang + "\n************'; cd " + lang + "; " + cmd_build + " cd ..; "
-  os.system(cmd)
+  print ""
+  print "****************"
+  print "Building " + lang
+  print "****************"
+  if clean:
+    cmd("rm -rf " + lang)
+    cmd("mkdir " + lang)
   
+  # BUILD PDF
+  
+  # Silly workarond to include the more or less correct PDF download link in the PDF
+  cmd("cp -f " + lang +"/latex/LVGL.pdf LVGL.pdf | true")
+  cmd("BUILDDIR=\"" + lang + "\" make -j8 -e SPHINXOPTS=\"-D language='" + lang + "'\"  latex")
+  # Generat PDF
+  cmd("cd " + lang + "/latex && xelatex -interaction=batchmode *.tex")
+  # Copy the result PDF to the main diractory to make it avaiable for the HTML build
+  cmd("cd " + lang + "/latex && cp -f LVGL.pdf ../../LVGL.pdf")
+
+  # BULD HTML
+  cmd("BUILDDIR=\"" + lang + "\" make -j8 -e SPHINXOPTS=\"-D language='" + lang + "'\" html")
   
