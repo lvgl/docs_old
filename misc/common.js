4399ba9d525eebff9977345a0b9bc732841af20a
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-06-19 22:21:10 +0200

    add dev

--- misc/common.js
+++ misc/common.js
@@ -4,6 +4,7 @@ function add_version_selector()
   p.innerHTML = `
 <select name="versions" id="versions" onchange="ver_sel()" style="border-radius:5px; margin-bottom:15px">
   <option value="latest">latest</option>
+  <option value="dev">development</option>
   <option value="v7">release/v7</option>
   <option value="v6">release/v6</option>
 </select>` + p.innerHTML;
