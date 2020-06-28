478f2a512fd1ed200b8d7e30417ccdf6afccda2f
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-06-28 13:54:27 +0200

    add update script

---
+++ update.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+ 
+import sys
+import os
+
+langs = ['en']
+
+  
+def cmd(s):
+  print("============================================")
+  print(s)
+  print("============================================")
+  r = os.system(s)
+  if r != 0: 
+    print "Exit build due to previous error"
+    exit(-1)
+    
+    
+
+br = "latest"
+tmpdir = "_docs_tmp_" + br
+
+cmd("git co " + br)
+cmd("git clean -fd")
+cmd("git submodule update" + br)
+cmd("./build.py")
+cmd("rm -fr ../" + tmpdir)
+cmd("cp en ../" + tmpdir)
+cmd("git co master")
+cmd("git clean -fd")
+cmd("cp -a ../" + tmpdir + "/. " + br)
+
