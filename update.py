176ef8e9603f42f81634af663ef0647698d43c4e
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-06-29 12:31:30 +0200

    fix update.py

--- update.py
+++ update.py
@@ -20,34 +20,33 @@ def cmd(s):
 br_arr = sys.argv[1:]
     
 for br in br_arr:
-	br = "release/v7"
-	tmpdir = "_docs_tmp_" + br
-	urlpath = re.sub('release/', '', br)
-
+  br = "release/v7"
+  tmpdir = "_docs_tmp_" + br
+  urlpath = re.sub('release/', '', br)
 
   print("")
   print("Update " + br)
   print("========================")
 
 
-	cmd("git co " + br)
-	cmd("git clean -fd")
-	cmd("git pull origin " + br)
-	cmd("git submodule update")
-	cmd("./build.py")
-	cmd("rm -fr ../" + tmpdir)
+  cmd("git co " + br)
+  cmd("git clean -fd")
+  cmd("git pull origin " + br)
+  cmd("git submodule update")
+  cmd("./build.py")
+  cmd("rm -fr ../" + tmpdir)
 
-	for l in langs:
-		if os.path.isdir("./" + l):
-		  cmd("cp -a " + l +" ../" + tmpdir)
-		
-	cmd("git co master")
-	cmd("git clean -fd")
-	
-	for l in langs:
-		cmd("rm -rf " + br + "/" + l)
-	
-	cmd("cp -a ../" + tmpdir + "/. " + br)
-	cmd("git ci -am 'Update " + urlpath + "'")
-	
+  for l in langs:
+    if os.path.isdir("./" + l):
+      cmd("cp -a " + l +" ../" + tmpdir)
+    
+  cmd("git co master")
+  cmd("git clean -fd")
+  
+  for l in langs:
+    cmd("rm -rf " + br + "/" + l)
+  
+  cmd("cp -a ../" + tmpdir + "/. " + br)
+  cmd("git ci -am 'Update " + urlpath + "'")
+  
 cmg("git push origin master")
