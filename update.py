948969eb855cdd219a253b7264e96115bbafa441
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-06-29 12:29:48 +0200

    fix update.py

--- update.py
+++ update.py
@@ -8,35 +8,46 @@ langs = ['en']
 
   
 def cmd(s):
-  print("============================================")
+  print("")
   print(s)
-  print("============================================")
+  print("----------------------------")
   r = os.system(s)
   if r != 0: 
     print "Exit build due to previous error"
     exit(-1)
     
     
+br_arr = sys.argv[1:]
+    
+for br in br_arr:
+	br = "release/v7"
+	tmpdir = "_docs_tmp_" + br
+	urlpath = re.sub('release/', '', br)
 
-br = "release/v7"
-tmpdir = "_docs_tmp_" + br
-urlpath = re.sub('release/', '', br)
 
-cmd("git co " + br)
-cmd("git clean -fd")
-cmd("git pull origin " + br)
-cmd("git submodule update")
-cmd("./build.py")
-cmd("rm -fr ../" + tmpdir)
+  print("")
+  print("Update " + br)
+  print("========================")
 
-for l in langs:
-  if os.path.isdir("./" + l):
-    cmd("cp -a " + l +" ../" + tmpdir)
-  
-cmd("git co master")
-cmd("git clean -fd")
-cmd("cp -a ../" + tmpdir + "/. " + br)
-cmd("git ci -am 'Update " + urlpath + "'")
+
+	cmd("git co " + br)
+	cmd("git clean -fd")
+	cmd("git pull origin " + br)
+	cmd("git submodule update")
+	cmd("./build.py")
+	cmd("rm -fr ../" + tmpdir)
+
+	for l in langs:
+		if os.path.isdir("./" + l):
+		  cmd("cp -a " + l +" ../" + tmpdir)
+		
+	cmd("git co master")
+	cmd("git clean -fd")
+	
+	for l in langs:
+		cmd("rm -rf " + br + "/" + l)
+	
+	cmd("cp -a ../" + tmpdir + "/. " + br)
+	cmd("git ci -am 'Update " + urlpath + "'")
+	
 cmg("git push origin master")
-
-
