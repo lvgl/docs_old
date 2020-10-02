aa45cb8fec25d94554fc0602e9ccde9e00f35cb2
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-10-02 20:41:41 +0200

    Update header.rst

--- build.py
+++ build.py
@@ -50,9 +50,9 @@ cmd("cp -f " + lang +"/latex/LVGL.pdf LVGL.pdf | true")
 cmd("sphinx-build -b latex . en/latex")
 
 # Generat PDF
-#cmd("cd " + lang + "/latex && xelatex -interaction=batchmode *.tex")
+cmd("cd " + lang + "/latex && xelatex -interaction=batchmode *.tex")
 # Copy the result PDF to the main diractory to make it avaiable for the HTML build
-#cmd("cd " + lang + "/latex && cp -f LVGL.pdf ../../LVGL.pdf")
+cmd("cd " + lang + "/latex && cp -f LVGL.pdf ../../LVGL.pdf")
 
 # BULD HTML
 cmd("sphinx-build -b html . en/html")
