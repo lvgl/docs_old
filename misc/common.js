cb2da53ef143f44dd92b94f55eea36b11c18f9b4
embeddedt <42941056+embeddedt@users.noreply.github.com>
2021-02-01 17:25:08 +0000

    Deploying to master from  @ 0001e257f01b523a7026c660769d339ee81fc4a8 🚀

---
+++ misc/common.js
@@ -0,0 +1,23 @@
+function add_version_selector()
+{
+  p = document.getElementById("rtd-search-form").parentElement;
+  p.innerHTML = `
+<select name="versions" id="versions" onchange="ver_sel()" style="border-radius:5px; margin-bottom:15px">
+  <option value="latest">latest</option>
+  <option value="dev">development</option>
+  <option value="v7">release/v7</option>
+  <option value="v6">release/v6</option>
+</select>` + p.innerHTML;
+}
+
+function ver_sel()
+{
+  var x = document.getElementById("versions").value;
+  window.location.href = window.location.protocol + "//" + window.location.host + "/" + x + "/en/html";
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    add_version_selector();
+    var value = window.location.pathname.split('/')[1];
+    document.getElementById("versions").value = value;
+})
